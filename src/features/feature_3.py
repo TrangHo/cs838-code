4e154a5f27537de3d429ddc488136cb8d3624db8
Thomas Ngo <thomasngo@Thomass-MacBook-Pro.local>
2017-02-23 19:34:45 -0600

    Upload first 5 features

---
+++ src/features/feature_3.py
@@ -0,0 +1,25 @@
+# FEATURE 3 : CONTAIN COLLEGE
+#
+# TO CHECK IF AN EXTRACTED STRING CONTAINS THE WORD "College" OR NOT
+#     TRUE - CONTAINS "College"
+#     FALSE - OTHERWISE
+#
+#====================================================================================================
+
+def relevant(word):
+    if (word == "of") or (word == "at") or (word == "in") or (word == "for"):
+        return False
+    else:
+        return True
+
+def preprocessing(s):
+    s = s.replace(",", " ").replace("-", " ").replace(".", " ")
+
+def feature3(s):
+    words = s.split()
+    toreturn = False
+    for word in words:
+        if (word == 'College'):
+            toreturn = True
+            break
+    return toreturn

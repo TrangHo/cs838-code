4304ebdc9c8cd8771efc754e7a219e3c34fdb8a0
Qinyuan Sun <Ken@Qinyuans-MacBook-Pro.local>
2017-02-24 18:01:45 -0600

    feature 10

---
+++ src/lib/features/feature_8.py
@@ -0,0 +1,6 @@
+import re
+from lib.constants import university_nouns
+# Whether it has one these prefixes: "attend(ed)"
+def test(str, prefix):
+    pattern = re.compile('(graduated|graduate|graduating|graduates) from$')
+    return re.search(pattern, prefix)

c7e9fd1476fc4678ec829eaa6edf7aebffa3a353
TrangHo <carepjan@KuroiTamago.local>
2017-02-24 22:57:29 -0600

    Finish positiveExampleGenerator + csv files for positive examples

--- src/lib/featureGenerator.py
+++ src/lib/featureGenerator.py
@@ -22,6 +22,7 @@ class FeatureGenerator:
     self.prefix = self.__get_prefix(text, begin, end)
     self.suffix = self.__get_suffix(text, begin, end)
     self.features = self.__generate_features()
+
     return dict(
       str = self.str,
       prefix = self.prefix,
@@ -30,13 +31,13 @@ class FeatureGenerator:
     )
 
   def __get_prefix(self, text, begin, end):
-    # (\\t|\s|\w|\d|\'|,|-)*
+    # (\\t|\s|\w|\d|\'|,|-|:|;)*
     # Regex which matches all strings which contains tab, space, \w, \d, apostrophe, comma, dash
 
     # (?:\s+(([A-Z]|[A-Z][a-z](?:[A-Z])?)\.)+([A-Z]|[A-Z][a-z](?:[A-Z])?)(?:\.)?\s+)?
     # Regex which match M.B.A or M.B.A. or PhD. this regex is optional in the pattern
     # this regex might not match case like this: He received a M.B.A and a Ph.D.
-    pattern = re.compile('(\.|\\n)(\\t|\s|\w|\d|\'|,|-)*(?:\s+(([A-Z]|[A-Z][a-z](?:[A-Z])?)\.)+([A-Z]|[A-Z][a-z](?:[A-Z])?)(?:\.)?\s+)?(\\t|\s|\w|\d|\'|,|-)*' + text[begin:end] + '$')
+    pattern = re.compile('(\.|\\n)(\\t|\s|\w|\d|\'|,|-|:|;)*(?:\s+(([A-Z]|[A-Z][a-z](?:[A-Z])?)\.)+([A-Z]|[A-Z][a-z](?:[A-Z])?)(?:\.)?\s+)?(\\t|\s|\w|\d|\'|,|-|:|;)*' + text[begin:end] + '$')
     prefix = text[0:end]
     match = re.search(pattern, prefix)
     if match is not None:
@@ -48,7 +49,7 @@ class FeatureGenerator:
     return prefix
 
   def __get_suffix(self, text, begin, end):
-    pattern = re.compile('^' + text[begin:end] + '((\\t|\s|\w|\d|\'|,|-)*(?:\s+(([A-Z]|[A-Z][a-z](?:[A-Z])?)\.)+([A-Z]|[A-Z][a-z](?:[A-Z])?)(?:\.)?)?)+(\\t|\s|\w|\d|\'|,|-)*((\.\s+[A-Z])|(\s*\\n)|(\.\s*\'(?:\')?))')
+    pattern = re.compile('^' + text[begin:end] + '(\\t|\s|\w|\d|\'|,|-|:|;)*(?:\s+(([A-Z]|[A-Z][a-z](?:[A-Z])?)\.)+([A-Z]|[A-Z][a-z](?:[A-Z])?)(?:\.)?)?(\\t|\s|\w|\d|\'|,|-|:|;)*((\.\s+[A-Z])|(\s*\\n)|(\.\s*\'(?:\')?))')
     suffix = text[begin:len(text)]
     match = re.search(pattern, suffix)
     if match is not None:
---
+++ src/lib/positiveExamplesGenerator.py
@@ -0,0 +1,58 @@
+import os
+import re
+import arff
+import csv
+from lib.featureGenerator import FeatureGenerator
+
+class PositiveExamplesGenerator:
+  def __init__(self, dir_path):
+    self.dir_path = dir_path
+    self.count = 0
+    self.generator = FeatureGenerator()
+    self.data = []
+    self.metadata = []
+
+    self.__scan_directory()
+
+  def examples(self):
+    return dict(
+      data = self.data,
+      metadata = self.metadata
+    )
+
+  def export_to_csvs(self):
+    with open('postiveExamples.csv', 'w', newline='') as csvfile:
+      writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+      for vector in self.data:
+        writer.writerow(vector)
+
+    with open('postiveExamplesMetadata.csv', 'w', newline='') as csvfile:
+      writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+      for vector in self.metadata:
+        writer.writerow(vector)
+
+  def __scan_directory(self):
+    for root, dirs, files in os.walk(self.dir_path):
+      for file in files:
+        if file.endswith('.txt'):
+          filepath = self.dir_path + '/' + file
+          text = open(filepath).read()
+          pattern = r'<pos>(\w|-|,|\.|\s)+</pos>'
+          text_len = len(text)
+
+          for markup in re.finditer(pattern, text):
+            # truncate text to before the markup 500 chars and after markup 500 chars
+            # to help speedup finding prefix and suffix as well as increasing its
+            # accuracy.
+            context_start = markup.start() - 500 if markup.start() > 500 else 0
+            context_end = markup.end() + 500 if markup.end() + 500 < text_len else text_len - 1
+            if markup.start() > 500:
+              start = markup.start() - context_start
+              end = markup.end() - context_start
+            else:
+              start = markup.start()
+              end = markup.end()
+
+            res = self.generator.generate_features(text[context_start:context_end], start, end)
+            self.data.append(res['features'])
+            self.metadata.append([file, res['str'], res['prefix'], res['suffix']])

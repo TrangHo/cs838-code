afb2b246ab0b99babc753b0513d1d2d66366ac64
TrangHo <carepjan@KuroiTamago.local>
2017-02-25 21:32:44 -0600

    Fix __fit_without_cross_validations

--- src/lib/classifier.py
+++ src/lib/classifier.py
@@ -6,11 +6,13 @@
 # logistic regression
 import csv
 from statistics import mean
+import numpy as np
 
 from sklearn import svm, tree
+from sklearn.neural_network import MLPClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LinearRegression, LogisticRegression
-from sklearn.model_selection import cross_val_score, cross_val_predict, train_test_split
+from sklearn.model_selection import cross_val_score, cross_val_predict, train_test_split, StratifiedShuffleSplit, KFold
 from sklearn.metrics import precision_score, recall_score
 
 from lib.constants import constants
@@ -44,6 +46,8 @@ class Classifier:
     #   self.cls = LinearRegression()
     elif classifier_type == constants.CLASSIFIERS['LOGISTIC_REGRESSION']:
       self.cls = LogisticRegression()
+    elif classifier_type == constants.CLASSIFIERS['NEURAL_NETWORK']:
+      self.cls = MLPClassifier()
 
   def __parse_data(self, filename, label):
     with open(filename, newline='') as csvfile:
@@ -62,6 +66,7 @@ class Classifier:
     else: return 0
 
   def __fit_with_cross_validation(self, fold=constants.CV_FOLD):
+    fold = KFold(constants.CV_FOLD, shuffle=True)
     precisions = cross_val_score(self.cls, self.vectors, self.labels, cv=fold, scoring='precision')
     recalls = cross_val_score(self.cls, self.vectors, self.labels, cv=fold, scoring='recall')
     predictions = cross_val_predict(self.cls, self.vectors, self.labels, cv=fold)
@@ -75,7 +80,24 @@ class Classifier:
     )
 
   def __fit_without_cross_validation(self, test_size = constants.DEV_TEST_SIZE_RATIO):
-    X_train, X_test, y_train, y_test = train_test_split(self.vectors, self.labels, test_size)
+    sss = StratifiedShuffleSplit(n_splits=1, test_size=test_size, random_state=0)
+    sss.get_n_splits(self.vectors, self.labels)
+    X_train = list()
+    y_test = list()
+    X_test = list()
+    y_test = list()
+
+    for train_idx, test_idx in sss.split(self.vectors, self.labels):
+      train_index = train_idx
+      test_index = test_idx
+
+    vectors = np.array(self.vectors)
+    labels = np.array(self.labels)
+    X_train, X_test = vectors[train_index], vectors[test_index]
+    y_train, y_test = labels[train_index], labels[test_index]
+
+
+    # X_train, X_test, y_train, y_test = train_test_split(self.vectors, self.labels, test_size)
     self.cls.fit(X_train, y_train)
     predictions = self.cls.predict(X_test)
     precision = precision_score(y_test, predictions)
